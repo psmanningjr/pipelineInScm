299ca20c0fbf3f771ea0d56a99284d8ed447459c
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-20 17:00:03 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -24,6 +24,7 @@ parameters: [
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.           
+      sh "ls -tal"
       }
    }
   
