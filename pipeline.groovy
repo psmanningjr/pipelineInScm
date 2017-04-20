f0e228c033a8ae3eae7f666570378b00462c0931
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-20 16:56:34 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,7 +16,16 @@ parameters: [
 
   ]
 
- 
+ node {
+   def mvnHome
+   stage('Preparation') { // for display purposes
+      // Get some code from a GitHub repository
+      git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+      // Get the Maven tool.
+      // ** NOTE: This 'M3' Maven tool must be configured
+      // **       in the global configuration.           
+     
+   }
   
   node('maven') {
      sh "echo ${stuff}"
