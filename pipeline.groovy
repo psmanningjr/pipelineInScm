ece8debac0ba00127c6e6685c4e180616fa62bc2
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-20 19:45:33 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -21,7 +21,7 @@ parameters: [
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
       //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
-      git url:'http://t978659@mintgitlab.syngentaaws.org/mint/material.git', branch: 'openshift', credentialsId: 'paulgitlab'
+      git branch: 'openshift', credentialsId: 'paulgitlab', url:'http://t978659@mintgitlab.syngentaaws.org/mint/material.git', 
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.   
