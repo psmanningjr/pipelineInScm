2003768ac8a49165c52f0946bfeee692c72fe54d
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-21 08:06:28 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -79,7 +79,8 @@ sh "echo 'oc process $TEMPLATE_NAME -n syngenta RUNTIME=$RUNTIME HOSTNAME_HTTP=$
  // sh "oc process ${TEMPLATE_NAME} -n syngenta RUNTIME=${RUNTIME} HOSTNAME_HTTP=${HOSTNAME_HTTP}"
  // sh "echo oc project ${TO_NAMESPACE}"
 //# Get parameters expected by template
-TEMPLATE_VARS= sh "oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters"
+TEMPLATE_VARS_WITH_DEFS= sh "oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d ' ' | tail -n +2"
+  TEMPLATE_VARS = sh ''
   sh 'echo ${TEMPLATE_VARS}'
 //  TEMPLATE_PARAMS= (oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d &quot; &quot; | tail -n +2).execute.text
   sh " echo ${TEMPLATE_PARAMS}"
