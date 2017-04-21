7964326e0bf4541b58cc9b5d4ae691b7ec58e7e7
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-21 15:42:10 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
 
-//input message: 'message', ok: 'do it', parameters: [choice(choices: "red\ngreen\npurple\n'], description: '', name: 'color')
 stuff = input message: 'Select Promotion Parameters', 
 parameters: [
   choice(choices: "mint-dev\nmint-test\nmint-stage\nmint-system-integration\nmint-training\nmint-prod", description: '', name: 'from'),
@@ -19,12 +18,12 @@ parameters: [
 
 
 node {
-  sh "echo ${stuff}"
+  //sh "echo ${stuff}"
   FROM_TAG = stuff.get('fromTag')
-APP_REPO = stuff.get('appRepo')
-APP_BRANCH = stuff.get('APP_BRANCH')
-CONFIG_REPO = stuff.get('configRepo')
-CONFIG_BRANCH = stuff.get('CONFIG_BRANCH')
+  APP_REPO = stuff.get('appRepo')
+  APP_BRANCH = stuff.get('APP_BRANCH')
+  CONFIG_REPO = stuff.get('configRepo')
+  CONFIG_BRANCH = stuff.get('CONFIG_BRANCH')
   TEMPLATE_NAME = stuff.get('templateName')
   RUNTIME = stuff.get('RUNTIME')
   APP_NAME = stuff.get('app_name')
@@ -33,100 +32,50 @@ CONFIG_BRANCH = stuff.get('CONFIG_BRANCH')
   TEMPLATE_NAME = stuff.get('templateName')
   HOSTNAME_HTTP = stuff.get('HOSTNAME_HTTP')
   
-  sh "echo config_repo = ${CONFIG_REPO}"
-  sh "echo to= ${TO_NAMESPACE}"
-//CONF_REPO = stuff[configRepo]
-//CONF_BRANCH = stuff[CONFIG_BRANCH]
- //  def mvnHome
- //  stage('Preparation') { // for display purposes
- //     // Get some code from a GitHub repository
- //     //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
- //     git branch: 'master', credentialsId: 'mint-dev-jenkinsgitlabsecret', url:'git@10.127.183.7:openshift/dev-configs.git' 
- //     // Get the Maven tool.
- //     // ** NOTE: This 'M3' Maven tool must be configured
- //     // **       in the global configuration.   
- //    //mvnHome = tool 'maven'
- //     sh "ls -tal"
- //     }
- //  }
-  
-  //node('maven') {
-   //  sh "echo ${stuff}"
-
-     //sh "echo"
-    
-    //sh "ls -tal"
-   // checkout scm
-   // sh "ls -tal"
-    //scmdump = scm.dump()
-    //sh 'echo scm dump = ${scmdump}'
-   // sh 'mvn --version'
-  //}
-  //node('master') {
-   
-  // sh "ls -tal"
-   //sh "ls -tal /tmp"
-    dir( 'config_repo' ) { 
+  dir( 'config_repo' ) { 
       git branch: CONFIG_BRANCH, credentialsId: 'mint-dev-jenkinsgitlabsecret', url: CONFIG_REPO
         sh 'ls -tal'
-    }
+  }
     
-    dir ( 'app_repo' ) { 
+  dir ( 'app_repo' ) { 
       git branch: APP_BRANCH, credentialsId: 'mint-dev-jenkinsgitlabsecret', url: APP_REPO
         sh 'ls -tal'
-     }
+  }
 
   sh "oc process ${TEMPLATE_NAME} -n syngenta RUNTIME=${RUNTIME} HOSTNAME_HTTP=${HOSTNAME_HTTP} >/tmp/toprocess"
   sh "oc apply -f /tmp/toprocess -n ${TO_NAMESPACE}"
  
   sh "oc project ${TO_NAMESPACE}"
-//# Get parameters expected by template
-  sh "ls -tal >adir"
-  def dir = readFile('adir').trim()
-  println " my dir output $dir"
-sh script: "oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters >/tmp/paraminfo "
-def info = readFile('/tmp/paraminfo').trim()
-  println " paraminfo = ${info}"
+  
+  //# Get parameters expected by template
+  sh script: "oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters >/tmp/paraminfo "
   sh "cut -f 1 -d "+'" "' + " /tmp/paraminfo >/tmp/onlynames"
   sh "tail -n +2 /tmp/onlynames >/tmp/tailed"
+
   def TEMPLATE_PARAMS = readFile('/tmp/tailed').trim()
-  println "TEMPLATE_PARAMS = ${TEMPLATE_PARAMS}"
-//  TEMPLATE_PARAMS= (oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d &quot; &quot; | tail -n +2).execute.text
-  
- 
+  println "Template parameters = ${TEMPLATE_PARAMS}"
+
   //# Filter out unneeded config arguments
-// org TEMPLATE_ARGS=$(for item in $TEMPLATE_PARAMS; do printf &quot;$(grep ^$item= config_repo/vars.sh) &quot;; done)
-  //List lines = TEMPLATE_PARAMS.split( '\n' ).findAll
-  //String[] splitData = TEMPLATE_PARAMS.split("\n");
- //sh "cut -f 1 -d "+'"="' + " config_repo/vars.sh >/tmp/configVarNames"
- //   def configVars = readFile('/tmp/configVarNames') 
-   def TEMPLATE_ARGS =""
+  def TEMPLATE_ARGS =""
   def configVars = readFile('config_repo/vars.sh')
   String[] splitData = configVars.split("\n");
   count = splitData.size()
-  println "size of splitData = ${count}"
-  println "config vars = ${configVars}"
-  //println "configvars = ${configVars}"
+  //println "config vars = ${configVars}"
   for (String eachSplit : splitData) {
-    println "processing ${eachSplit}"
+    //println "processing ${eachSplit}"
     indexOfEquals = eachSplit.indexOf("=")
     if (indexOfEquals > -1 ) {
       compare = eachSplit.substring(0,indexOfEquals)
       println "compare with ${compare}"
       if(TEMPLATE_PARAMS.contains(compare)){
           println "found match"
-        TEMPLATE_ARGS = TEMPLATE_ARGS + '"' + eachSplit + '" '
+          TEMPLATE_ARGS = TEMPLATE_ARGS + '"' + eachSplit + '" '
       }
     }
-   }
-
-
-//  sh "for item in ${TEMPLATE_PARAMS}; do printf " + '"' + " returnitem(item) " +'"' + "; done > /tmp/template.args"
-//  def TEMPLATE_ARGS = readFile('/tmp/template.args')
-println "args = $TEMPLATE_ARGS"
-sh "oc process --namespace=${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml ${TEMPLATE_ARGS} >/tmp/configmap"
+  }
+  //println "args = $TEMPLATE_ARGS"
+  
+  sh "oc process --namespace=${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml ${TEMPLATE_ARGS} >/tmp/configmap"
   sh "oc apply -f /tmp/configmap --namespace=${TO_NAMESPACE}"
   sh "oc tag ${FROM_NAMESPACE}/${APP_NAME}:${FROM_TAG} ${TO_NAMESPACE}/${APP_NAME}:latest"
 }
-
-
