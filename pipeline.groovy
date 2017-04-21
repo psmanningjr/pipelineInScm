1546a3126e461742a9313b673b838e6ca52b2455
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-20 23:23:21 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -80,7 +80,7 @@ CONFIG_BRANCH = stuff.get('CONFIG_BRANCH')
  // sh "echo oc project ${TO_NAMESPACE}"
 //# Get parameters expected by template
   TEMPLATE_PARAMS= sh "oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d &quot; &quot; | tail -n +2"
-  sh ' echo ${TEMPLATE_PARAMS}'
+  sh " echo ${TEMPLATE_PARAMS}"
   //# Filter out unneeded config arguments
 //TEMPLATE_ARGS=$(for item in $TEMPLATE_PARAMS; do printf &quot;$(grep ^$item= config_repo/vars.sh) &quot;; done)
 //  TEMPLATE_ARGS= sh 'for item in $TEMPLATE_PARAMS; do printf &quot; returnitem(item) &quot;; done'
