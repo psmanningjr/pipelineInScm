06045c8da381be7aaf19a721ec21c0ecabf35135
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-21 11:44:52 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -78,7 +78,8 @@ CONFIG_BRANCH = stuff.get('CONFIG_BRANCH')
   
   sh 'oc project ${TO_NAMESPACE}'
 //# Get parameters expected by template
-sh 'oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d " " | tail -n +2'
+sh 'oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters'
+//sh 'oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d " " | tail -n +2'
 //  TEMPLATE_PARAMS= (oc process --namespace ${TO_NAMESPACE} -f app_repo/openshift-config-map-template.yml --parameters | cut -f 1 -d &quot; &quot; | tail -n +2).execute.text
   
  // sh " echo ${TEMPLATE_PARAMS}"
