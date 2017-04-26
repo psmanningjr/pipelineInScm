d5bfaa91e71533276aebd9dc56241c6ed2a05a7e
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-26 14:38:47 -0400

    Update testing.groovy

--- testing.groovy
+++ testing.groovy
@@ -53,7 +53,7 @@ node {
   
   sh "cat ./listOfNamesFromTemplate "
   def templateNames = readFile('./listOfNamesFromTemplate').trim()
-  def TEMPLATE_PARAMS = takeFirstFieldOnEachItem(dropFirstItem(templateNames))
+  def TEMPLATE_PARAMS = takeFirstFieldOnEachItem(templateNames.removeElement(0)))
 //  sh "cut -f 1 -d "+'" "' + " ./listOfNamesFromTemplate >/tmp/onlynames"
 //  sh "cat /tmp/onlynames"
 //  sh "tail -n +2 /tmp/onlynames >/tmp/tailed"
