c45a66732694cf9fc95d6ee30aefcf8c1f8d1b47
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-26 13:22:10 -0400

    Update testing.groovy

--- testing.groovy
+++ testing.groovy
@@ -17,21 +17,6 @@ parameters: [
   ]
 
 node {
-  stage('test') {
-   def whatThe = someFunc('textToFunc')
-   def whatThe2 = someFunc2('textToFunc2')
-  }
-}
-
-def someFunc(String text){
-    echo text
-    text
-}
-def someFunc2(String text2){
-    echo text2
-    text2
-}
-node {
   stage('testing') {
   //sh "echo ${stuff}"
   FROM_TAG = stuff.get('fromTag')
